7dd3f1976ecdad6787337023f9963061081ad7ef
Mateusz <sshozye@gmail.com>
2025-09-30 21:19:37 +0200

    made memory asv regression for tracking serialization

--- statistics_asv/regression.py
+++ statistics_asv/regression.py
@@ -2,7 +2,9 @@
 # This file is used for checking if something changed between commits
 # It tracks accuracy, time and memory used by sketches
 from functools import cache
+import pickle
 import random
+import sys
 from typing import Callable, Union
 import numpy as np
 
@@ -51,9 +53,10 @@ class RegressionSuite:
         self.filled_instance = self.cls_factory(M_SIZE, get_seeds(M_SIZE))
         self.filled_instance.add_many(self.elems, self.weights)
 
-    def mem_instance(self, cached_data, impl_name):
-        print(self.fresh_instance.__getstate__())
-        return self.fresh_instance.__getstate__
+    def track_serialization_size(self, cached_data, impl_name):
+        serialized_object = pickle.dumps(self.filled_instance)
+        return len(serialized_object)
+    track_serialization_size.unit = 'bytes' # type: ignore
     
     def time_add_many(self, cached_data, impl_name):
         self.fresh_instance.add_many(self.elems, self.weights)
@@ -74,10 +77,9 @@ class RegressionSuite:
         mean_estimate = np.mean(estimates)
         error = abs(mean_estimate - self.true_cardinality) / self.true_cardinality
         return error * 100
+    track_relative_error.unit = '%' # type: ignore
 
     def track_variance(self, cached_data, impl_name):
         estimates = self._get_estimates()
         return np.var(estimates)
-    
-    track_relative_error.unit = '%' # type: ignore
     track_variance.unit = 'value^2' # type: ignore 
