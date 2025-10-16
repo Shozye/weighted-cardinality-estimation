4be48ec20b9d5341fd0c731a6f1e8d4252b40cf7
Mateusz <sshozye@gmail.com>
2025-10-16 13:55:55 +0200

    improvement: using mersenne twister as rng in fisher yates

--- src/weighted_cardinality_estimation/fisher_yates.cpp
+++ src/weighted_cardinality_estimation/fisher_yates.cpp
@@ -3,18 +3,20 @@
 #include<cmath>
 
 FisherYates::FisherYates(std::uint32_t sketch_size):
+      rng_engine(std::random_device{}()),
       permInit(static_cast<std::uint32_t>(std::ceil(std::log2(sketch_size))), sketch_size),
       permWork(static_cast<std::uint32_t>(std::ceil(std::log2(sketch_size))), sketch_size) {
     std::iota(permInit.begin(), permInit.end(), 1);
 }
 
 void FisherYates::initialize(std::uint64_t rng_seed){
-    this->rng_seed = rng_seed;
+    this->rng_engine.seed(rng_seed);
     permWork = permInit;
 }
 
 std::uint32_t FisherYates::get_fisher_yates_element(uint32_t index){
-    uint32_t r = rand(index, this->permInit.size());
+    std::uniform_int_distribution<uint32_t> dist(index, this->permInit.size() - 1);
+    uint32_t r = dist(this->rng_engine);
     std::uint32_t swap = permWork[index];
     permWork[index] = permWork[r];
     permWork[r] = swap;
@@ -22,15 +24,8 @@ std::uint32_t FisherYates::get_fisher_yates_element(uint32_t index){
     return j;
 }
 
-uint32_t FisherYates::rand(uint32_t min, uint32_t max){
-    this->rng_seed = this->rng_seed * 1103515245 + 12345;
-    auto temp = (unsigned)(this->rng_seed/65536) % 32768;
-    return (temp % (max-min)) + min;
-}
-
 std::uint32_t FisherYates::bytes_total() const {
     size_t total_size = 0;
-    total_size += sizeof(rng_seed); // 8
     total_size += permInit.bytes(); // m * ceil(log_2 m)
     total_size += permWork.bytes(); // m * ceil(log_2 m)
     return total_size; // 2m ceil(log_2 m) + 8
@@ -38,7 +33,6 @@ std::uint32_t FisherYates::bytes_total() const {
 
 std::uint32_t FisherYates::bytes_write() const {
     size_t total_size = 0;
-    total_size += sizeof(rng_seed); // 8
     total_size += permWork.bytes(); // m * ceil(log_2 m)
     return total_size; // m ceil(log_2 m) + 8
 }
--- src/weighted_cardinality_estimation/fisher_yates.hpp
+++ src/weighted_cardinality_estimation/fisher_yates.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <compact_vector.hpp>
 #include <cstdint>
+#include <random>
 
 class FisherYates {
 public:
@@ -12,7 +13,7 @@ public:
     std::uint32_t bytes_write() const ;
     std::uint32_t bytes_total() const ;
 private:
-    uint64_t rng_seed = 0; 
+    std::mt19937_64 rng_engine;
     uint32_t rand(uint32_t min, uint32_t max);
     compact::vector<uint32_t> permInit; // static structure, only used to fast copy to permWork
     compact::vector<uint32_t> permWork; // used at the beginning of every update
